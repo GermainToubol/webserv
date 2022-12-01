2df2f12da63291df28b81c683d90e36483ce5b56
Floran Mauguin <fmauguin.pro@gmail.com>
2022-12-01 16:04:49 +0100

    Saving

--- srcs/Server/Server_cgi.cpp
+++ srcs/Server/Server_cgi.cpp
@@ -6,9 +6,14 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:18:26 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/12/01 15:21:21 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/12/01 16:03:24 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <algorithm>
+
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include "WebServer.hpp"
 
@@ -21,5 +26,45 @@ bool	WebServer::isCgi(int file_fd)
 
 int	WebServer::cgiResponse(int file_fd)
 {
+	int	client_fd;
+	int	readed;
+	int	sended;
+	Response *response;
+	std::string	buf;
+	std::string::size_type	end;
+
+	client_fd = this->_cgiFD.find(file_fd)->second;
+	response = this->getResponse(client_fd);
+
+	if (response->getStatus() == 0 || response->getHeader() == "")
+	{
+		sended = send(client_fd, response->getHeader().c_str(), std::min((size_t)SEND_SIZE, response->getHeader().size()), MSG_NOSIGNAL | MSG_MORE);
+		if (sended == -1)
+		{
+			this->removeResponse(client_fd);
+			close(file_fd);
+			return (0);
+		}
+		if (sended >= response->getHeader().size())
+		{
+			response->setStatus(1);
+			response->setHeader("");
+		}
+		else
+			response->eraseHeader(0, sended);
+	}
+	if (response->getStatus() == 1)
+	{
+		readed = recv(file_fd, this->_buffer, BUFFER_SIZE, MSG_NOSIGNAL);
+		if (readed == 0 || readed == -1)
+		{
+			this->removeResponse(client_fd);
+			close(file_fd);
+			return (0);
+		}
+		buf = std::string(this->_buffer, readed);
+		if ((end = buf.find("\n\n") != std::string::npos) || (end = buf.find("\r\n\r\n") != std::string::npos))
+			response->setHeader(response->getHeader() + buf.substr(0, end));
+	}
 
 }
